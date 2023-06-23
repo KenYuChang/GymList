bb63bc1e22a0c75cd4b85a102edec2d836716535
KenYuChang <justin0915justin0915@gmail.com>
2023-06-23 15:32:33 +0800

    feat:admin-edit & delete

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -41,5 +41,47 @@ const adminController = {
       res.render('admin/show', { gym })
     } catch (err) {}
   },
+  editGym: async (req, res, next) => {
+    try {
+      const id = req.params.id
+      const gym = await Gym.findByPk(id, {
+        raw: true,
+      })
+      if (!gym) throw new Error("Gym didn't exist!")
+      res.render('admin/edit-gym', { gym })
+    } catch (err) {
+      next(err)
+    }
+  },
+  putGym: async (req, res, next) => {
+    try {
+      const { name, tel, address, openingHours, description } = req.body
+      if (!name) throw new Error('Gym name is required')
+      const gym = await Gym.findByPk(req.params.id)
+      if (!gym) throw new Error("Gym didn't exist")
+      await gym.update({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+      })
+      req.flash('success_messages', 'Gym was successfully created')
+      res.redirect('/admin/gym')
+    } catch (err) {
+      next(err)
+    }
+  },
+  deleteGym: async (req, res, next) => {
+    try {
+      const id = req.params.id
+      const gym = await Gym.findByPk(id)
+      if (!gym) throw new Error("Gym didn't exist")
+      await gym.destroy()
+      res.redirect('/admin/gym')
+    } catch (err) {
+      next(err)
+    }
+  },
 }
 module.exports = adminController
