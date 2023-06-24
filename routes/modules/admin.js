d1178ede6f7a9819e9bb208aeff830eaeef50cc5
KenYuChang <justin0915justin0915@gmail.com>
2023-06-24 15:33:45 +0800

    feat:category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,7 @@ const adminController = require('../../controllers/admin-controller')
 const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 
+//gym
 router.get('/gym/create', adminController.createGym)
 router.get('/gym/:id/edit', adminController.editGym)
 router.get('/gym/:id', adminController.getGym)
@@ -11,13 +12,17 @@ router.put('/gym/:id', upload.single('image'), adminController.putGym)
 router.delete('/gym/:id', adminController.deleteGym)
 router.get('/gym', adminController.getAdminPage)
 router.post('/gym', upload.single('image'), adminController.postGym)
+
+//user
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
 //分類
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteGym)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
+
 router.get('/', (req, res) => res.redirect('/admin/gym'))
 module.exports = router
