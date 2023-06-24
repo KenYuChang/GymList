cf78ebf2a479db931a16ba7f727940cc3b3d5a92
KenYuChang <justin0915justin0915@gmail.com>
2023-06-24 15:45:48 +0800

    feat: add index home page

--- controllers/gym-controller.js
+++ controllers/gym-controller.js
@@ -1,6 +1,22 @@
+const { Gym, Category } = require('../models')
 const gymController = {
-  getHomePage: (req, res, next) => {
-    return res.render('gym')
+  getHomePage: async (req, res, next) => {
+    try {
+      const gyms = await Gym.findAll({
+        include: [Category],
+        raw: true,
+        nest: true,
+      })
+      const data = gyms.map((g) => ({
+        ...g,
+        description: g.description.substring(0, 50),
+      }))
+      res.render('gym', {
+        gyms: data,
+      })
+    } catch (err) {
+      next(err)
+    }
   },
 }
 module.exports = gymController
